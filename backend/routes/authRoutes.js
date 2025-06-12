45175d97987fb2832771aa509d9e3f3b7f5a444a
RAHUL SAINI <110321253+Rahulsaini27@users.noreply.github.com>
2025-06-12 11:38:36 +0530

    Update authRoutes.js for testing admin

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,12 +12,13 @@ const { authMiddleware } = require('../middlewares/authMiddleware');
 
 // Public routes
 router.post('/login', login);
+router.post('/register', register);
 
 // Middleware to protect below routes
 router.use(authMiddleware);
 
 // Protected routes
-router.post('/register', register);
+// router.post('/register', register);
 router.get('/get-all', getUsers);
 router.put('/update/:id', updateUser);
 router.delete('/delete/:id', deleteUser);
